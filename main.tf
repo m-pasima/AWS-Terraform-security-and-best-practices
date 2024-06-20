a07288cfb5b9afd927ba16378515c1f09974dbb7
m-pasima <theivykierachronicles@gmail.com>
2024-06-20 01:14:16 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -103,38 +103,3 @@ resource "aws_instance" "terraform" {
     Name = "terraform-test"
   }
 }
-
-# Create an S3 Bucket with versioning and default server-side encryption
-resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "secure_sec_config"
-
-  versioning {
-    enabled = true
-  }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-
-  tags = {
-    Name = "SecureBucket"
-  }
-
-  # tfsec:ignore:aws-s3-encryption-customer-key
-  # tfsec:ignore:aws-s3-enable-bucket-logging
-}
-
-resource "aws_s3_bucket_public_access_block" "public_access_block" {
-  bucket = aws_s3_bucket.s3_bucket.id
-
-  block_public_acls   = true
-  block_public_policy = true
-  ignore_public_acls  = true
-  restrict_public_buckets = true
-}
-
-
